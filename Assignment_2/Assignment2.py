4828073f5b340e1b3bd08420c43b69b98f962fed
aa2673 <aa2673@kent.ac.uk>
2026-03-26 23:40:14 +0000

    Character creation function, class adventurer

--- Assignment_2/Assignment2.py
+++ Assignment_2/Assignment2.py
@@ -3,7 +3,7 @@ import time
 
 def main():
     loading_screen()
-    character_name()
+    character_creation()
 
 def loading_screen():
     time.sleep(1)
@@ -23,16 +23,46 @@ def loading_screen():
     return
 
 
+class Adventurer:
+    def __init__(self,name,off,vit,agl):
+        self.name = name
+        self.off = off
+        self.vit = vit
+        self.agl = agl
 
-def character_name():
+
+def character_creation():
     while True:
         name = input("\nWelcome to Aterra Online! Please enter a first and last name for your adventurer: ").strip().title()
         if re.search(r"^[a-zA-Z]+\s+[a-zA-Z]", name):
-            print("Nice to meet you", name)
-            return name
+            print(f"Nice to meet you {name}!")
+            print("How would you like to allocate your stat points? You have 20 to spend.")
+            offense = int(input("Offense: "))
+            vitality = int(input("Vitality: "))
+            agility = int(input("Agility: "))
+            adventurer = Adventurer(name, offense, vitality, agility)
+            print(f"\n---Adventurer Profile---\nName:{name}\nOffense:{offense}\nVitality:{vitality}\nAgility:{agility}")
+            return adventurer
             break
         else:
             print("\nFirst and last names must only contain letters a-z and should have a space between them.")
+        
+
+        
+
+
+    
+
+        
+
+
+#
+# class Classes:
+    
+    
+
+
+
 
     
 
